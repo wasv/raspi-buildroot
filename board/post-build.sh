077411e7399094eef38f5cc2a8aa19266d6438b9
Billy Stevens <contact@wasv.me>
2020-03-15 21:43:24 -0400

    Initial Commit

---
+++ board/post-build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -u
+set -e
+
+# Add a console on tty1
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+fi
+ln -sfv /usr/lib/systemd/system/getty@.service ${TARGET_DIR}/etc/systemd/system/getty.target.wants/getty@tty1.service
---
+++ external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_RPI_MUSIC_HUB_PATH)/package/*/*.mk))
