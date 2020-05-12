5510dba188b429e3a64d7d31373196d26413c672
Billy Stevens <contact@wasv.me>
2020-05-12 14:17:16 -0400

    post-build: Mount /boot using /etc/fstab

--- board/post-build.sh
+++ board/post-build.sh
@@ -13,3 +13,6 @@ ln -sfv /usr/lib/systemd/system/getty@.service ${TARGET_DIR}/etc/systemd/system/
 
 # Disable wpa_supplicant.service, rootfs enables wpa_supplicant@wlan0.service
 rm -f ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/wpa_supplicant.service
+
+# Add /boot to /etc/fstab
+echo /dev/mmcblk0p1 /boot auto rw 0 0 >> ${TARGET_DIR}/etc/fstab
