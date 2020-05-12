8d84ded77e77ff30e8fc2c532e8ae75d5e34ce1a
Billy Stevens <contact@wasv.me>
2020-05-12 13:40:33 -0400

    Added wireless autostart to rootfs overlay.

--- board/post-build.sh
+++ board/post-build.sh
@@ -10,3 +10,6 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 ln -sfv /usr/lib/systemd/system/getty@.service ${TARGET_DIR}/etc/systemd/system/getty.target.wants/getty@tty1.service
+
+# Disable wpa_supplicant.service, rootfs enables wpa_supplicant@wlan0.service
+rm -f ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/wpa_supplicant.service
