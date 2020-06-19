0c89092441ac3176a3c6a50e1414ca8be235a6c2
Billy Stevens <contact@wasv.me>
2020-06-19 17:01:37 -0400

    Uses symlinks to identify boards.

--- board/raspberrypi/post-image.sh
+++ board/raspberrypi/post-image.sh
@@ -3,7 +3,7 @@
 set -e
 
 BOARD_DIR="$(dirname $0)"
-BOARD_NAME="raspberrypi0w"
+BOARD_NAME="$(basename $BOARD_DIR)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
